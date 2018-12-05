5358b152238be835f5c6be0a09b0a0b2f2b1c521
Nick <dacosta.nick@yahoo.com>
2018-12-05 01:04:00 -0500

    Started Project
    Added starting classes

---
+++ Project1DataStructuresFinalProject/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Node.h"
+#include "AStar.h"
+
+int main()
+{
+	
+
+
+	// Wait for user to enter a character to pause the console.
+	char userInput = NULL;
+
+	std::cout << "Please enter any character to continue..." << std::endl;
+	std::cin >> userInput;
+
+	return 0;
+}
